a81da8d8d8261d1f03b24b2ebde4cd133000b3d3
Pedro BM <p.bonilla.miralles@gmail.com>
2019-03-26 16:35:05 +0100

    handle functions

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  
+  goToStore = (event) => {
+    event.preventDefault();
+    const storeName = this.myInput.current.value;
+    console.log("a",this.props)
+    this.props.history.push(`/store/${storeName}`)
+    console.log("b",this.props.history.push)
+
+  }
+  
   render() {
+    
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Score</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text"
+        ref={this.myInput}
+        required placeholder="Store Name" />
         <button type="submit">Visit Store -></button>
       </form>
     )
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import StorePicker from './components/StorePicker';
-import App from "./components/App";
+// import StorePicker from './components/StorePicker';
+// import App from "./components/App";
 import Router from './components/Router';
 import './css/style.css';
 
